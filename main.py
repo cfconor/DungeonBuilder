07f31c07d0261a20754d359fc593b88c8b4c0c21
whoiscray <cfconor@gmail.com>
2025-03-27 17:58:25 +0000

    fixed issues with POST arguments

--- main.py
+++ main.py
@@ -5,28 +5,23 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    rooms = str(escape(request.args.get("rooms","")))
+    #rooms = str(escape(request.args.get("rooms","")))
     return (
-        """<form actio  n="" method="get">
-                <input type="text" name="rooms">
-                <input type="submit" value="Generate">
-              </form>"""
-        + rooms
+        """<h1>Index</h1>"""
     ) 
     
 @app.route("/rooms", methods=['GET','POST'])
 def generateDungeon():
+  if request.method == 'POST':
+    rooms = request.form.get('rooms')
+    #print("Rooms: " + rooms)
+    if rooms.strip() == '':
+      return "<h1>Invalid Number, please enter number</h1>"
     
+    print("Generate Dungeon Here")
+    return render_template('result.html', rooms=rooms)
 
-    if request.method == 'POST':
-      rooms = request.args.get('rooms')
-      if rooms.strip() == '':
-        return "<h1>Invalid Number, please enter number</h1>"
-      
-      print("Generate Dungeon Here")
-      return render_template('result.html')
-
-    return render_template('rooms.html')
+  return render_template('rooms.html')
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=8080, debug=True)
