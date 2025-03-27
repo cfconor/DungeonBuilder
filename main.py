a727c590ddf7115aa3f88b6cd7090fc76b623361
whoiscray <cfconor@gmail.com>
2025-03-27 16:12:50 +0000

    added initial form for choosing number of rooms

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
 from flask import Flask
-from flask import request, escape
-
-
+from flask import request, escape, render_template
 
 app = Flask(__name__)
 
-
 @app.route("/")
-
 def index():
     rooms = str(escape(request.args.get("rooms","")))
     return (
@@ -18,6 +14,19 @@ def index():
         + rooms
     ) 
     
+@app.route("/rooms", methods=['GET'])
+def generateDungeon():
+    rooms = request.args.get('rooms')
+
+    if rooms is None: #No number entered, show input form
+        return render_template('rooms.html')
+    elif rooms.strip() == '':
+        return "<h1>Invalid Number, please enter number</h1>"
+    try:
+        print("Generate Dungeon Here")
+        return render_template('result.html')
+    except ValueError:
+        return "<h1>Couldn't generate dungeon</h1>"
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=8080, debug=True)
