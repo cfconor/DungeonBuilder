120860793dc538c6327ce79fe00a8e75c74e8a93
whoiscray <cfconor@gmail.com>
2025-03-27 14:18:20 +0000

    added form to index and function to grab rooms value

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from flask import Request
+
 
 
 app = Flask(__name__)
@@ -7,9 +9,15 @@ app = Flask(__name__)
 @app.route("/")
 
 def index():
-
-    return "Congratulations, it's a web app!"
-
+    rooms = Request.args.get("rooms","")
+    return (
+        """<form action="" method="get">
+                <input type="text" name="rooms">
+                <input type="submit" value="Generate">
+              </form>"""
+        + rooms
+    ) 
+    
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=8080, debug=True)
